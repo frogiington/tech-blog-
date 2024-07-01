4b7483b6a9f511b6ab5ee6a94445e891d971507d
Alyson Sanchez <coqui0429@gmail.com>
2024-07-01 14:19:39 -0400

    edited __public, render attempt 4

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const controller = require('./Controller');
 const sequelize = require('./config/connection.js');
+const public = require (__public);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // turn on controller
 app.use(controller);
-app.use(express.static(__public));
+app.use(express.static(public));
 //TO DO: create public folder for everything to be available to the client
 
 sequelize.authenticate()
