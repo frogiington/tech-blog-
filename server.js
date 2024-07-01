a3dd964b3c43c0e2e0b4b79f5a80d49c96ca3ebf
Alyson Sanchez <coqui0429@gmail.com>
2024-07-01 16:03:34 -0400

    port fixes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const sequelize = require('./config/connection.js');
 //const public = require('./public');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
